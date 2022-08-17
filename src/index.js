6cbc323c830291629fb0aee7963ad4efa6e9a522
Bailey Cage <bailey.cage@gmail.com>
2022-08-17 10:39:52 -0400

    display ramen static details on click

--- src/index.js
+++ src/index.js
@@ -15,6 +15,25 @@ function displayRamenImages (ramenAry) {
     ramenAry.forEach(ramen => {
         const menuEntryImg = document.createElement('img');
         menuEntryImg.src = ramen.image;
+        menuEntryImg.id = ramen.id;
         menu.append(menuEntryImg);
+        menuEntryImg.addEventListener('click', displayRamenDetails);
+    });
+}
+
+function displayRamenDetails (event) {
+    const ramenId = event.target.id;
+
+    const details = document.querySelector('#ramen-detail');
+    const ramenImg = details.querySelector('img.detail-image');
+    const ramenName = details.querySelector('h2.name');
+    const ramenRestaurant = details.querySelector('h3.restaurant');
+    
+    fetch (`${URL}/${ramenId}`)
+    .then (response => response.json())
+    .then (ramenEntry => {
+        ramenImg.src = ramenEntry.image;
+        ramenName.textContent = ramenEntry.name;
+        ramenRestaurant.textContent = ramenEntry.ramenRestaurant;
     })
 }
